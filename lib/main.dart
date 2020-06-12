fcf57b775f36026e6b0db6dc3e4622df8dc35905
Agrim Gupta <agrim121@gmail.com>
2020-06-12 17:32:48 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:World_time_app/pages/home.dart';
+import 'package:flutter/material.dart';
+import 'package:World_time_app/pages/home.dart';
+
+void main() {
+  runApp(MaterialApp(home: Home(),));
+}
+
